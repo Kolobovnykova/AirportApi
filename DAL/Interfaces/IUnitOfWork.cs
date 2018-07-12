fa61dedc52ad35189c9958848489a6cf3994e345
Anastasiia <nastena31011993@gmail.com>
2018-07-12 19:11:06 +0300

    Implemented unit of work

--- DAL/Interfaces/IUnitOfWork.cs
+++ DAL/Interfaces/IUnitOfWork.cs
@@ -3,12 +3,9 @@ using DAL.Models;
 
 namespace DAL.Interfaces
 {
-    public interface IUnitOfWork 
+    public interface IUnitOfWork
     {
-        IRepository<TEntity> Set<TEntity>() where TEntity: Entity;
-
         int SaveChages();
-
         Task<int> SaveChangesAsync();
     }
 }
