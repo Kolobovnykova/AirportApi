61daa6529af50a1dc66d89dca6333bdad816dafe
Anastasiia <nastena31011993@gmail.com>
2018-07-16 23:40:32 +0300

    Added api test

---
+++ AirportApi.Tests/ApiTests/ApiTests.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using Shared.DTOs;
+using static Shared.RequestHelpers.RequestHelper;
+
+namespace AirportApi.Tests.ApiTests
+{
+    [TestFixture]
+    public class ApiTests
+    {
+        private const string Url = @"http://localhost:52062/api/";
+
+        [Test]
+        public void Test()
+        {
+            // Arrange
+            // Act
+            var response = GetRequest(Url + "crews");
+            var crew = JsonConvert.DeserializeObject<CrewDTO[]>(response);
+            // Assert
+            Assert.IsInstanceOf<CrewDTO[]>(crew);
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            // Arrange
+            var responseList = GetRequest(Url + "crews");
+            var crewList = JsonConvert.DeserializeObject<CrewDTO[]>(responseList);
+            var id = crewList[0].Id;
+            
+            // Act
+            var response = GetRequest(Url + "crews/" + id);
+            var crew = JsonConvert.DeserializeObject<CrewDTO>(response);
+            
+            // Assert
+            Assert.IsInstanceOf<CrewDTO>(crew);
+        }
+    }
+}
---
+++ Shared/RequestHelpers/RequestHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Net;
+using System.Text;
+
+namespace Shared.RequestHelpers
+{
+    public static class RequestHelper
+    {
+        public static string GetRequest(string url, string id = null)
+        {
+            var request = (HttpWebRequest) WebRequest.Create(url);
+
+            request.Method = "GET";
+            request.Accept = "application/json";
+
+            WebResponse response = request.GetResponse();
+            using (Stream responseStream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
