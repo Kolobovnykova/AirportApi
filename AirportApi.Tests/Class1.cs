943fdc678c4acbe6ae423dd92952c20a40803716
Anastasiia <nastena31011993@gmail.com>
2018-07-16 13:52:58 +0300

    Added tests project

---
+++ AirportApi.Tests/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AirportApi.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        // Arrange
+        
+        // Act
+
+        // Assert
+    }
+}
